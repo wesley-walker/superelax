8cb563231996a160f015fe8d3a7269e2ca521475
Wesley Walker <wesleywalker.code@gmail.com>
2024-02-23 22:16:17 -0500

    vercel ready

--- django1/settings.py
+++ django1/settings.py
@@ -78,12 +78,7 @@ WSGI_APPLICATION = "django1.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {}
 
 
 # Password validation
